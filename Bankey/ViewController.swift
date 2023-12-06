43578e4246ac0b5f8674be108eb504a5d9836116
mariam <mariamsikandari22@example.com>
2023-12-06 09:57:39 -0500

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Mariam Sikandari on 2023-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
