c9973c16ec1de4da3867aadc39814901b878c547
mariam <mariamsikandari22@example.com>
2023-12-17 08:26:11 -0500

    ShakeyBell

--- Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -11,6 +11,8 @@ class AccountSummaryHeaderView : UIView {
     
     @IBOutlet var contentView: UIView!
     
+    let shakeyBellView = ShakeyBellView()
+    
     override init(frame: CGRect) {
            super.init(frame: frame)
            commonInit()
@@ -25,16 +27,29 @@ class AccountSummaryHeaderView : UIView {
            return CGSize(width: UIView.noIntrinsicMetric, height: 144)
        }
        
-       private func commonInit() {
-           let bundle = Bundle(for: AccountSummaryHeaderView.self)
-           bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
-           addSubview(contentView)
-           contentView.backgroundColor = appColor
-           
-           contentView.translatesAutoresizingMaskIntoConstraints = false
-           contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-           contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-           contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-           contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-       }
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+      
+        setupShakeyBell()
+        
+    }
+    
+    private func setupShakeyBell() {
+        shakeyBellView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(shakeyBellView)
+        NSLayoutConstraint.activate([
+            shakeyBellView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shakeyBellView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
    }
